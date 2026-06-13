e27b00fead314e8a4fa23e48197987b54a008972
RANGAM JAGADESH <jashu9392@gmail.com>
2026-06-13 13:54:27 +0530

    Phase 2: Added Persistent Storage

--- app/store.py
+++ app/store.py
@@ -1,9 +1,26 @@
+import json
+import os
+
+
 class KeyValueStore:
+
     def __init__(self):
-        self.data = {}
+        self.file_name = "storage.json"
+        self.data = self.load_data()
+
+    def load_data(self):
+        if os.path.exists(self.file_name):
+            with open(self.file_name, "r") as file:
+                return json.load(file)
+        return {}
+
+    def save_data(self):
+        with open(self.file_name, "w") as file:
+            json.dump(self.data, file, indent=4)
 
     def set(self, key, value):
         self.data[key] = value
+        self.save_data()
 
     def get(self, key):
         return self.data.get(key)
@@ -11,6 +28,7 @@ class KeyValueStore:
     def delete(self, key):
         if key in self.data:
             del self.data[key]
+            self.save_data()
             return True
         return False
 
